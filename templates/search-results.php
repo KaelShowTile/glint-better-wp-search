a229e806b065352b0e69cab03a5909690a23bf44
KaelShowTile <kaelshowtile@gmail.com>
2026-01-20 10:15:44 +1100

    2026012002

--- templates/search-results.php
+++ templates/search-results.php
@@ -31,9 +31,9 @@ get_header(); ?>
                                 <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
                             </h2>
                         </header>
-
+                        
                         <div class="entry-summary">
-                            <?php the_excerpt(); ?>
+                            <?php //the_excerpt(); ?>
                         </div>
                     </article>
                 <?php endwhile; ?>
